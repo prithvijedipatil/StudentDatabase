61388b1932ce82a3b4ce3fe4cde9d5c7f4dc9242
prithvijedipatil <prithvipatil09@gmail.com>
2023-07-04 15:18:48 +0530

    submissionn

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const database = [
-    {
-      ID: 1,
-      name: 'Alice',
-      age: 21,
-      grade: 9.2,
-      degree: 'Btech',
-      email: 'alice@example.com'
-    }
-   
-  ];
+  {
+    ID: 1,
+    name: "Alice",
+    age: 21,
+    grade: 9.2,
+    degree: "Btech",
+    email: "alice@example.com",
+  },
+];
 
-  
-  function displaytable() {
-    const studentsBody = document.getElementById('table-body');
-    studentsBody.innerHTML = ''; // Clear the previous content
-  
-    for (const student of database) {
-      const row = document.createElement('tr');
-      row.innerHTML = `
+function displaytable() {
+  const studentsBody = document.getElementById("table-body");
+  studentsBody.innerHTML = ""; // Clear the previous content
+
+  for (const student of database) {
+    const row = document.createElement("tr");
+    row.innerHTML = `
         <td>${student.ID}</td>
         <td>${student.name}</td>
         <td>${student.age}</td>
@@ -30,31 +28,33 @@ const database = [
          
         </td>
       `;
-  
-      studentsBody.appendChild(row);
-    }
-  }
 
-  const studentForm = document.getElementById('student-form');
-const submitButton = document.getElementById('submit-btn');
-const editButtons = document.getElementsByClassName('edit-btn');
-const deleteButtons = document.getElementsByClassName('delete-btn');
+    studentsBody.appendChild(row);
+  }
+}
+
+const studentForm = document.getElementById("student-form");
+const submitButton = document.getElementById("submit-btn");
+const editButtons = document.getElementsByClassName("edit-btn");
+const deleteButtons = document.getElementsByClassName("delete-btn");
 
 let isEditing = false;
 let editStudentId = null;
 
-studentForm.addEventListener('submit', function(event) {
+studentForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const name = document.getElementById('name').value;
-  const age = document.getElementById('age').value;
-  const grade = document.getElementById('grade').value;
-  const degree = document.getElementById('degree').value;
-  const email = document.getElementById('email').value;
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const grade = document.getElementById("grade").value;
+  const degree = document.getElementById("degree").value;
+  const email = document.getElementById("email").value;
 
   if (isEditing) {
     // Edit student
-    const editedStudent = database.find(student => student.ID === editStudentId);
+    const editedStudent = database.find(
+      (student) => student.ID === editStudentId
+    );
     if (editedStudent) {
       editedStudent.name = name;
       editedStudent.age = age;
@@ -70,14 +70,14 @@ studentForm.addEventListener('submit', function(event) {
       age,
       grade,
       degree,
-      email
+      email,
     };
     database.push(newStudent);
   }
 
   // Reset the form
   studentForm.reset();
-  submitButton.textContent = 'Add Student';
+  submitButton.textContent = "Add Student";
   isEditing = false;
   editStudentId = null;
 
@@ -85,17 +85,17 @@ studentForm.addEventListener('submit', function(event) {
 });
 
 // Event delegation for edit buttons
-document.addEventListener('click', function(event) {
-  if (event.target.classList.contains('edit-btn')) {
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("edit-btn")) {
     const studentId = parseInt(event.target.dataset.id);
-    const student = database.find(student => student.ID === studentId);
+    const student = database.find((student) => student.ID === studentId);
     if (student) {
-      document.getElementById('name').value = student.name;
-      document.getElementById('age').value = student.age;
-      document.getElementById('grade').value = student.grade;
-      document.getElementById('degree').value = student.degree;
-      document.getElementById('email').value = student.email;
-      submitButton.textContent = 'Edit Student';
+      document.getElementById("name").value = student.name;
+      document.getElementById("age").value = student.age;
+      document.getElementById("grade").value = student.grade;
+      document.getElementById("degree").value = student.degree;
+      document.getElementById("email").value = student.email;
+      submitButton.textContent = "Edit Student";
       isEditing = true;
       editStudentId = studentId;
     }
@@ -103,10 +103,12 @@ document.addEventListener('click', function(event) {
 });
 
 // Event delegation for delete buttons
-document.addEventListener('click', function(event) {
-  if (event.target.classList.contains('delete-btn')) {
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-btn")) {
     const studentId = parseInt(event.target.dataset.id);
-    const studentIndex = database.findIndex(student => student.ID === studentId);
+    const studentIndex = database.findIndex(
+      (student) => student.ID === studentId
+    );
     if (studentIndex !== -1) {
       database.splice(studentIndex, 1);
       displaytable();
@@ -114,26 +116,27 @@ document.addEventListener('click', function(event) {
   }
 });
 
-const searchInput = document.getElementById('search-input');
+const searchInput = document.getElementById("search-input");
 
-searchInput.addEventListener('input', function(event) {
+searchInput.addEventListener("input", function (event) {
   const searchTerm = event.target.value.toLowerCase();
 
-  const filteredStudents = database.filter(student =>
-    student.name.toLowerCase().includes(searchTerm) ||
-    student.email.toLowerCase().includes(searchTerm) ||
-    student.degree.toLowerCase().includes(searchTerm)
+  const filteredStudents = database.filter(
+    (student) =>
+      student.name.toLowerCase().includes(searchTerm) ||
+      student.email.toLowerCase().includes(searchTerm) ||
+      student.degree.toLowerCase().includes(searchTerm)
   );
 
   displaytable(filteredStudents);
 });
 
 function displaytable(filteredStudents = database) {
-  const studentsBody = document.getElementById('table-body');
-  studentsBody.innerHTML = ''; // Clear the previous content
+  const studentsBody = document.getElementById("table-body");
+  studentsBody.innerHTML = ""; // Clear the previous content
 
   for (const student of filteredStudents) {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
     row.innerHTML = `
       <td>${student.ID}</td>
       <td>${student.name}</td>
@@ -148,7 +151,6 @@ function displaytable(filteredStudents = database) {
         
       </td>
     `;
-    
 
     studentsBody.appendChild(row);
   }
